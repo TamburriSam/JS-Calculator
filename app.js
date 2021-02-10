f72d09f15aa803a24e6cdb47dc8d82bab0dbfd84
tamburrisam <tamburrisam@gmail.com>
2021-02-10 12:11:24 -0500

    added finishing touches

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function populateDisplay(e){
 
         displayScreen.innerHTML = displayValueOne + operatorValue// + e.target.textContent;
 
-      return  displayValueTotal = displayScreen.innerHTML
+   return  displayValueTotal = displayScreen.innerHTML
 
  }
 
@@ -155,4 +155,67 @@ let total = '';
 
 
 equalsButton.addEventListener('click', sum);
+
+
+
+
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+const darkHeading = document.querySelector('.dark');
+
+const getStyle = (element, style) => 
+  window
+    .getComputedStyle(element)
+    .getPropertyValue(style);
+
+const initialColors = {
+  bg: getStyle(html, "--bg"),
+}
+
+const darkMode = {
+    bg: "#333333" // override styles here
+}
+
+const transformKey = key => 
+  "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColors = (colors) => {
+  Object.keys(colors).map(key => 
+    html.style.setProperty(transformKey(key), colors[key]) 
+  );
+}
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors);
+});
+
+const isExistLocalStorage = (key) => 
+  localStorage.getItem(key) != null;
+
+const createOrEditLocalStorage = (key, value) => 
+  localStorage.setItem(key, JSON.stringify(value));
+
+const getValeuLocalStorage = (key) =>
+  JSON.parse(localStorage.getItem(key));
+
+checkbox.addEventListener("change", ({target}) => {
+  if (target.checked) {
+    changeColors(darkMode);
+    createOrEditLocalStorage('mode','darkMode');
+  } else {
+    changeColors(initialColors);
+    createOrEditLocalStorage('mode','initialColors');
+  }
+})
+
+if(!isExistLocalStorage('mode'))
+  createOrEditLocalStorage('mode', 'initialColors');
+
+if (getValeuLocalStorage('mode') === "initialColors") {
+  checkbox.removeAttribute('checked');
+  changeColors(initialColors);
+} else {
+  checkbox.setAttribute('checked', "");
+  changeColors(darkMode);
+}
 
