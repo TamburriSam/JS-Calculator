3241d16292a48518e26ccdb781c29c8e44dd8a60
tamburrisam <tamburrisam@gmail.com>
2021-02-24 18:00:12 -0500

    updated format

--- app.js
+++ app.js
@@ -1,221 +1,201 @@
-let oneButton = document.querySelector('.one');
-let twoButton = document.querySelector('.two');
-let threeButton = document.querySelector('.three');
-let fourButton = document.querySelector('.four');
-let fiveButton = document.querySelector('.five');
-let sixButton = document.querySelector('.six');
-let sevenButton = document.querySelector('.seven');
-let eightButton = document.querySelector('.eight');
-let nineButton = document.querySelector('.nine');
-let zeroButton = document.querySelector('.zero')
+let oneButton = document.querySelector(".one");
+let twoButton = document.querySelector(".two");
+let threeButton = document.querySelector(".three");
+let fourButton = document.querySelector(".four");
+let fiveButton = document.querySelector(".five");
+let sixButton = document.querySelector(".six");
+let sevenButton = document.querySelector(".seven");
+let eightButton = document.querySelector(".eight");
+let nineButton = document.querySelector(".nine");
+let zeroButton = document.querySelector(".zero");
 
-let addButton = document.querySelector('.add');
-let subtractButton = document.querySelector('.subtract');
-let multiplyButton = document.querySelector('.multiply');
-let divideButton = document.querySelector('.divide');
+let addButton = document.querySelector(".add");
+let subtractButton = document.querySelector(".subtract");
+let multiplyButton = document.querySelector(".multiply");
+let divideButton = document.querySelector(".divide");
 
-let decButton = document.querySelector('.decimal');
-let clearButton = document.querySelector('.clear');
-let equalsButton = document.querySelector('.equals');
+let decButton = document.querySelector(".decimal");
+let clearButton = document.querySelector(".clear");
+let equalsButton = document.querySelector(".equals");
 
-let displayScreen = document.querySelector('.screen')
+let displayScreen = document.querySelector(".screen");
 
 //number events
-oneButton.addEventListener('click', populateDisplay);
-twoButton.addEventListener('click', populateDisplay)
-threeButton.addEventListener('click', populateDisplay)
-fourButton.addEventListener('click', populateDisplay)
-fiveButton.addEventListener('click', populateDisplay)
-sixButton.addEventListener('click', populateDisplay)
-sevenButton.addEventListener('click', populateDisplay)
-eightButton.addEventListener('click', populateDisplay)
-nineButton.addEventListener('click', populateDisplay)
-zeroButton.addEventListener('click', populateDisplay)
-
+oneButton.addEventListener("click", populateDisplay);
+twoButton.addEventListener("click", populateDisplay);
+threeButton.addEventListener("click", populateDisplay);
+fourButton.addEventListener("click", populateDisplay);
+fiveButton.addEventListener("click", populateDisplay);
+sixButton.addEventListener("click", populateDisplay);
+sevenButton.addEventListener("click", populateDisplay);
+eightButton.addEventListener("click", populateDisplay);
+nineButton.addEventListener("click", populateDisplay);
+zeroButton.addEventListener("click", populateDisplay);
 
 //operator events
-addButton.addEventListener('click', populateDisplay);
-subtractButton.addEventListener('click', populateDisplay)
-multiplyButton.addEventListener('click', populateDisplay)
-divideButton.addEventListener('click', populateDisplay)
+addButton.addEventListener("click", populateDisplay);
+subtractButton.addEventListener("click", populateDisplay);
+multiplyButton.addEventListener("click", populateDisplay);
+divideButton.addEventListener("click", populateDisplay);
 
 //special char events
-decButton.addEventListener('click', populateDisplay)
+decButton.addEventListener("click", populateDisplay);
 
-
-let displayValueTotal = '';
+let displayValueTotal = "";
 
 let displayValueOne = 0;
 let displayValueTwo = 0;
-let operatorValue = '';
+let operatorValue = "";
 
-let newTotal = ''
+let newTotal = "";
 
+function populateDisplay(e) {
+  let selectedNum = e.target.textContent;
 
+  displayValueTotal += selectedNum;
 
+  displayScreen.innerHTML += selectedNum;
 
-function populateDisplay(e){
-    
-    let selectedNum = e.target.textContent;
+  let splitValue = displayValueTotal.split(" ");
 
-    displayValueTotal+=selectedNum;
-    
-    displayScreen.innerHTML += selectedNum;
+  displayValueOne = splitValue[0];
+  operatorValue = splitValue[1];
+  displayValueTwo = splitValue[2];
 
-  
-    let splitValue = displayValueTotal.split(' ');
+  if (splitValue.length >= 5) {
+    sum();
 
-    displayValueOne = splitValue[0];
-    operatorValue = splitValue[1];
-    displayValueTwo = splitValue[2];
+    operatorValue = e.target.textContent;
 
+    displayScreen.innerHTML = displayValueOne + operatorValue; // + e.target.textContent;
 
-  if(splitValue.length>=5){
-
-    
-        sum(); 
-
-        operatorValue = e.target.textContent
-
-        displayScreen.innerHTML = displayValueOne + operatorValue// + e.target.textContent;
-
-   return  displayValueTotal = displayScreen.innerHTML
-
- }
-
+    return (displayValueTotal = displayScreen.innerHTML);
+  }
 }
 
-function clearDisplay(){
-    displayScreen.innerHTML = '';
-    displayValueTotal = 0;
-    displayValueOne = 0;
-    displayValueTwo = 0;
-    operatorValue = '';
-    total = 0;
+function clearDisplay() {
+  displayScreen.innerHTML = "";
+  displayValueTotal = 0;
+  displayValueOne = 0;
+  displayValueTwo = 0;
+  operatorValue = "";
+  total = 0;
 }
 
+clearButton.addEventListener("click", clearDisplay);
 
-clearButton.addEventListener('click', clearDisplay)
-
-function add(num1,num2){
-    return Math.abs(num1+num2)
+function add(num1, num2) {
+  return Math.abs(num1 + num2);
 }
 
-function subtract(num1,num2){
-    return num1-num2
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(num1, num2){
-    return num1 * num2
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(num1, num2){
-    return num1/num2
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
+let backBtn = document.querySelector(".backbutton");
 
-let backBtn = document.querySelector('.backbutton');
+backBtn.addEventListener("click", backSpace);
 
-backBtn.addEventListener('click', backSpace)
-
-function backSpace(e){
-    displayValueTotal = displayValueTotal.slice(0,-1);
-   displayScreen.innerText = displayScreen.innerText.slice(0, -1);
+function backSpace(e) {
+  displayValueTotal = displayValueTotal.slice(0, -1);
+  displayScreen.innerText = displayScreen.innerText.slice(0, -1);
 }
 
-
-function operate(displayValueOne, operatorValue, displayValueTwo){
-    if(operatorValue === '+'){
-      return  add(displayValueOne, displayValueTwo)
-    } else if (operatorValue === '-'){
-        return subtract(displayValueOne,displayValueTwo)
-    } else if (operatorValue === '*'){
-        return multiply(displayValueOne,displayValueTwo)
-    } else {
-        return divide(displayValueOne,displayValueTwo);   
-    }
+function operate(displayValueOne, operatorValue, displayValueTwo) {
+  if (operatorValue === "+") {
+    return add(displayValueOne, displayValueTwo);
+  } else if (operatorValue === "-") {
+    return subtract(displayValueOne, displayValueTwo);
+  } else if (operatorValue === "*") {
+    return multiply(displayValueOne, displayValueTwo);
+  } else {
+    return divide(displayValueOne, displayValueTwo);
+  }
 }
 
-function sum(){
+function sum() {
+  total = operate(
+    parseFloat(displayValueOne),
+    operatorValue,
+    parseFloat(displayValueTwo)
+  );
+  displayValueTotal = total;
+  displayValueOne = total;
 
-   total = operate(parseFloat(displayValueOne), operatorValue, parseFloat(displayValueTwo));
-    displayValueTotal = total;
-    displayValueOne = total;
+  if (
+    displayScreen.innerHTML === "Infinity" ||
+    displayScreen.innerHTML === "NaN"
+  ) {
+    return (displayScreen.innerHTML = "ERROR! YA DUN GOOFED");
+  }
 
-    if (displayScreen.innerHTML === 'Infinity' || displayScreen.innerHTML === 'NaN'){
-     return displayScreen.innerHTML = 'ERROR! YA DUN GOOFED';
-
-    }
-
-   return displayScreen.innerHTML = total;
+  return (displayScreen.innerHTML = total);
 }
 
-let total = '';
+let total = "";
 
+equalsButton.addEventListener("click", sum);
 
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input[name=theme]");
+const darkHeading = document.querySelector(".dark");
 
-equalsButton.addEventListener('click', sum);
-
-
-
-
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
-const darkHeading = document.querySelector('.dark');
-
-const getStyle = (element, style) => 
-  window
-    .getComputedStyle(element)
-    .getPropertyValue(style);
+const getStyle = (element, style) =>
+  window.getComputedStyle(element).getPropertyValue(style);
 
 const initialColors = {
   bg: getStyle(html, "--bg"),
-}
+};
 
 const darkMode = {
-    bg: "#333333" // override styles here
-}
+  bg: "#333333", 
+};
 
-const transformKey = key => 
+const transformKey = (key) =>
   "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
 
 const changeColors = (colors) => {
-  Object.keys(colors).map(key => 
-    html.style.setProperty(transformKey(key), colors[key]) 
+  Object.keys(colors).map((key) =>
+    html.style.setProperty(transformKey(key), colors[key])
   );
-}
+};
 
-checkbox.addEventListener("change", ({target}) => {
-    target.checked ? changeColors(darkMode) : changeColors(initialColors);
+checkbox.addEventListener("change", ({ target }) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors);
 });
 
-const isExistLocalStorage = (key) => 
-  localStorage.getItem(key) != null;
+const isExistLocalStorage = (key) => localStorage.getItem(key) != null;
 
-const createOrEditLocalStorage = (key, value) => 
+const createOrEditLocalStorage = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
-const getValeuLocalStorage = (key) =>
-  JSON.parse(localStorage.getItem(key));
+const getValeuLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
-checkbox.addEventListener("change", ({target}) => {
+checkbox.addEventListener("change", ({ target }) => {
   if (target.checked) {
     changeColors(darkMode);
-    createOrEditLocalStorage('mode','darkMode');
+    createOrEditLocalStorage("mode", "darkMode");
   } else {
     changeColors(initialColors);
-    createOrEditLocalStorage('mode','initialColors');
+    createOrEditLocalStorage("mode", "initialColors");
   }
-})
+});
 
-if(!isExistLocalStorage('mode'))
-  createOrEditLocalStorage('mode', 'initialColors');
+if (!isExistLocalStorage("mode"))
+  createOrEditLocalStorage("mode", "initialColors");
 
-if (getValeuLocalStorage('mode') === "initialColors") {
-  checkbox.removeAttribute('checked');
+if (getValeuLocalStorage("mode") === "initialColors") {
+  checkbox.removeAttribute("checked");
   changeColors(initialColors);
 } else {
-  checkbox.setAttribute('checked', "");
+  checkbox.setAttribute("checked", "");
   changeColors(darkMode);
 }
-
